2886319d2c4027bd9579177e73b97a0ec7340ca3
agovani <167785585+agovani@users.noreply.github.com>
2025-08-21 18:00:38 -0400

    Create lb.tf

---
+++ infra/Terraform/Staging/lb.tf
@@ -0,0 +1,27 @@
+resource "aws_lb" "this" {
+  name               = "${var.project}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+}
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.cloudflare_origin.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "${var.project}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip"
+}
