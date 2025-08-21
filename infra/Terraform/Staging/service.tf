b40cd7f0c0f3ac3a8523cc55df99eecb9c5b9aa2
agovani <167785585+agovani@users.noreply.github.com>
2025-08-21 18:02:40 -0400

    Create service.tf

---
+++ infra/Terraform/Staging/service.tf
@@ -0,0 +1,19 @@
+resource "aws_ecs_service" "Sparkrock_demo_services" {
+  name                = "SR_demo_first_services"
+  cluster             = aws_ecs_cluster.Spark_rock_demo_cluster.id
+  task_definition     = aws_ecs_task_definition.Sparkrock_demo_task.arn
+  launch_type         = "EC2"
+  scheduling_strategy = "REPLICA"
+  desired_count       = 1
+
+  network_configuration {
+    subnets          = [aws_default_subnet.sparkrock-test-subnet1.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "first-task"
+    container_port   = 80
+  }
+}
